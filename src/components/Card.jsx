baaf6d775c08e787b149ae8eb41271d0a8a6fd93
am <am@LAPTOP-GV30RNKF.localdomain>
2022-07-28 16:30:39 +0300

    NAVBAR

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,19 @@ import React from 'react';
 export default function Card({ card, setCards }) {
   return (
     <div
-      className="card col"
-      style={{ width: '18rem', backgroundColor: 'purple' }}
+      className="card"
+      style={{ backgroundColor: 'purple', width: 250 }}
     >
       <img src={`../../img/${card.img}`} className="card-img-top" alt="Картинка" />
-      <div className="card-body">
-        <h5 className="card-title">{card.nameCard}</h5>
+      <div className="card-body py-0">
+        <h5 className="card-title text-light">{card.nameCard}</h5>
+        <ul>
+          <li className="list-group-item text-info">{card.cost}$</li>
+          <li className="list-group-item text-info">Uniq: {card.status}</li>
+        </ul>
       </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">{card.cost}</li>
-        <li className="list-group-item">{card.status}</li>
-      </ul>
-      <div className="card-body">
-        <button type="button">Купить</button>
+      <div className="card-body py-0">
+        <button type="button" className='btn btn-warning'>Купить</button>
       </div>
     </div>
   );
